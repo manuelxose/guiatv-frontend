c4c8a21553db71d6d395949b8f8a873ae9eb52f0
mgonzalezv <mgonzalezv@eservicios.indra.es>
2025-12-05 00:14:09 +0100

    chore: ajusta gitignore y limpia artefactos

--- backend/dist/domain/repositories/IChannelRepository.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/v2/domain/repositories/IChannelRepository.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=IChannelRepository.js.map
--- backend/dist/v2/domain/repositories/IStorageRepository.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// src/v2/domain/repositories/IStorageRepository.ts
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=IStorageRepository.js.map
